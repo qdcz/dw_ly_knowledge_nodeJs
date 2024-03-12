ce95104e3e95c0002e60f7727d15842bbdb69f09
qdcz <532911513@qq.com>
2024-03-12 23:47:52 +0800

    'addExecCwd'

--- demo/process/use_childProcess/exec.js
+++ demo/process/use_childProcess/exec.js
@@ -9,3 +9,16 @@ exec('node ./child.js command001 command002:6666', (error, stdout, stderr) => {
     console.log(`stdout: ${stdout}`);
     console.error(`stderr: ${stderr}`);
 });
+
+
+// 进入到指定目录执行命令
+const targetDirectory = '/path/to/your/target/directory';
+const command = 'npm run build';
+exec(command, { cwd: targetDirectory }, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`执行出错： ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+});
